a08138db4f38043618948b30217056a48bd79fd1
Ileok <apr_3@naver.com>
2022-04-07 22:00:11 +0900

    Debug : MessageSQL

--- src/main/java/Talan/controller/MessageController.java
+++ src/main/java/Talan/controller/MessageController.java
@@ -5,11 +5,14 @@ import Talan.service.MessageService;
 import com.google.gson.Gson;
 import kr.msp.constant.Const;
 import oracle.net.ns.Message;
+
+import org.apache.ibatis.session.SqlSession;
 import org.json.JSONArray;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,6 +30,10 @@ public class MessageController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+	@Autowired(required = true)
+	@Qualifier("sqlSession_sample")
+	private SqlSession sqlSession;
+	
     @Autowired(required=true)
     private MessageService service;
 
@@ -76,6 +83,7 @@ public class MessageController {
 
         reqHeadMap.put(Const.RESULT_CODE, Const.OK);
         reqHeadMap.put(Const.RESULT_MESSAGE, Const.SUCCESS);
+        
 
         logger.info("======================= reqBodyMap : {}", reqBodyMap.toString());
 
